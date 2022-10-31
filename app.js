6b0a894c9db1471b1b76fc8442b85f310fccb394
Kelly <20522051@gm.uit.edu.vn>
2022-10-31 15:51:12 +0700

    base

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+require(
+    "dotenv"
+).config()
+const cors= require("cors")
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+const mongoose = require("mongoose")
+/* DB connection*/
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log(`DB connected ${mongoURI}`))
+  .catch((err) => console.log(err));
+
+app.use('/', indexRouter);
+
+
+module.exports = app;
